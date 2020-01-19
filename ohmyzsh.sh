b7806eebcc503714a64c2a9a1b6824f38ac453fb
Yokesh Thirumoorthi <yokeshthirumoorthi@gmail.com>
2020-01-19 03:21:39 +0000

    add zsh plugins

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -26,6 +26,23 @@ function install_ohmyzsh {
     wget –no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
     chsh -s /bin/zsh
     wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
+    # install oh-my-zsh plugins
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/themes/spaceship-prompt"
+    ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
+
+    git clone https://github.com/djui/alias-tips.git $HOME/.oh-my-zsh/custom/plugins/alias-tips
+    git clone https://github.com/supercrabtree/k $HOME/.oh-my-zsh/custom/plugins/k
+    git clone https://github.com/rupa/z $HOME/.oh-my-zsh/custom/plugins/z
+    git clone https://github.com/Valiev/almostontop.git $HOME/.oh-my-zsh/custom/plugins/almostontop
+
+    git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone  https://github.com/zsh-users/zsh-history-substring-search $HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+
+    wget -O $HOME/.zshrc https://gist.github.com/Yokeshthirumoorthi/3686d075a3da7b498343b91345b62c12/raw
+
     echo "Restart the system"
     echo "OhMyZSH Installed"
 }
