09f20e205e27c5151379d4426f2ca4405d3a1176
Yokesh Thirumoorthi <yokeshthirumoorthi@gmail.com>
2020-01-19 03:42:52 +0000

    remove photoprism install in do

--- digitalOcean.sh
+++ digitalOcean.sh
@@ -6,13 +6,11 @@ chmod +x ./update.sh
 chmod +x ./ohmyzsh.sh
 chmod +x ./languages.sh
 chmod +x ./git.sh
-chmod +x ./photoprism.sh
 
 yes | ./update.sh
 yes | ./ohmyzsh.sh
 yes | ./languages.sh
 yes | ./git.sh
-yes | ./photoprism.sh
 # yes | ./docker.sh
 # cd ~
 # source ~/.zshrc
