c1a173e49b265f046b830fad96c00ec4ef3fc8de
Yokesh Thirumoorthi <yokeshthirumoorthi@gmail.com>
2020-01-21 04:54:09 +0000

    install docker in digital ocean

--- digitalOcean.sh
+++ digitalOcean.sh
@@ -6,11 +6,12 @@ chmod +x ./update.sh
 chmod +x ./ohmyzsh.sh
 chmod +x ./languages.sh
 chmod +x ./git.sh
+chmod +x ./docker.sh
 
 yes | ./update.sh
 yes | ./ohmyzsh.sh
 yes | ./languages.sh
 yes | ./git.sh
-# yes | ./docker.sh
+yes | ./docker.sh
 # cd ~
 # source ~/.zshrc
